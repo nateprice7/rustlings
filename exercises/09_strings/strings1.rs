59da8c896481d88ccd4820d5b99e99ceac68e75f
Nathan Price <21696149+nateprice7@users.noreply.github.com>
2024-05-04 16:04:27 -0600

    finishing string section

--- exercises/09_strings/strings1.rs
+++ exercises/09_strings/strings1.rs
@@ -5,13 +5,16 @@
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let answer = current_favorite_color();
+    let answer2 = current_favorite_colorstr();
     println!("My current favorite color is {}", answer);
+    println!("My current favorite color is {}", answer2);
 }
 
-fn current_favorite_color() -> String {
+fn current_favorite_color() -> &'static str {
     "blue"
+}
+fn current_favorite_colorstr() -> String {
+    "blue".to_string()
 }
